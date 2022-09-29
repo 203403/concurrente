18724ab095e4b55063cd201cdd8c32857ae619ab
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-09-29 18:04:15 -0500

    Update examen.py

--- examen/examen.py
+++ examen/examen.py
@@ -3,14 +3,15 @@ import threading, time
 state = threading.Lock()
 
 def comer(id):
-    print("Persona " + str(id) + " tomo los dos palillos")
+    msg = "Persona " + str(id) + " tomo los dos palillos"
+    print("-" * len(msg) + "\n" + msg)
     list_1 = n_personas
     list_2 = [id]
     set_difference = set(list_1) - set(list_2)
     personas_esperando = list(set_difference)
-    print(f"Personas esperando: {personas_esperando}")
     print("Persona " + str(id) + " comiendo...", end = ' ')
-    time.sleep(2)
+    print(f"Personas esperando: {personas_esperando}")
+    
     
 class Persona(threading.Thread):
     def __init__(self, id, ):
@@ -20,8 +21,9 @@ class Persona(threading.Thread):
     def run(self):
         state.acquire()
         comer(self.id)
+        time.sleep(3)
         state.release()
-        print("Persona " + str(self.id) + "  termino de comer y dejo los palillos")
+        print("Persona " + str(self.id) + " termino de comer y dejo los palillos")
         
 
 n_personas=[1,2,3,4,5,6,7,8]
